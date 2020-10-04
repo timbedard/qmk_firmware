d68bfb2dba6340c208c361e7b1bc0e04533568f2
Tim Bedard <tpbedard@gmail.com>
2020-10-04 15:07:07 -0500

    make iris easier to read

--- keyboards/keebio/iris/keymaps/timbedard/config.h
+++ keyboards/keebio/iris/keymaps/timbedard/config.h
@@ -20,4 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define USE_I2C
 #define EE_HANDS
 
+#define PERMISSIVE_HOLD
+
 /* #define RGBLIGHT_LAYERS */
--- keyboards/keebio/iris/keymaps/timbedard/keymap.c
+++ keyboards/keebio/iris/keymaps/timbedard/keymap.c
@@ -6,6 +6,16 @@
 #define _RAISE 2
 #define _ADJUST 3
 
+#define KC_____ KC_TRNS
+#define KC_SPLW LT(_LOWER, KC_SPC)
+#define KC_SPRS LT(_RAISE, KC_SPC)
+#define KC_CESC CTL_T(KC_ESC)
+#define KC_SQUO SFT_T(KC_QUOT)
+#define KC_CALT C(KC_LALT)
+#define KC_ASPC A(KC_SPC)
+#define KC_XXXX KC_NO
+
+
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
@@ -15,36 +25,36 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_QWERTY] = LAYOUT(
-    KC_GRV, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_BSPC, \
-    KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, \
-    LCTL_T(KC_ESC), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT, \
-    KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_GRV, KC_DEL, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, RSFT_T(KC_QUOT), \
-    KC_LALT, KC_LGUI, LT(_LOWER, KC_SPC), LT(_RAISE, KC_SPC), C(KC_LALT), KC_ENT \
+  [_QWERTY] = LAYOUT_kc(
+     GRV,    1,    2,    3,    4,    5,                6,    7,    8,    9,    0, BSPC, \
+     TAB,    Q,    W,    E,    R,    T,                Y,    U,    I,    O,    P, BSLS, \
+    CESC,    A,    S,    D,    F,    G,                H,    J,    K,    L, SCLN,  ENT, \
+    LSFT,    Z,    X,    C,    V,    B,  GRV,  DEL,    N,    M, COMM,  DOT, SLSH, SQUO, \
+                            LALT, LGUI, SPLW, SPRS, CALT,  ENT \
   ),
 
-  [_LOWER] = LAYOUT(
-    KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PGUP, \
-    _______, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_PGDN, \
-    _______, _______, KC_LEFT, KC_RGHT, KC_UP, KC_LBRC, KC_RBRC, KC_P4, KC_P5, KC_P6, KC_PLUS, KC_HOME, \
-    _______, _______, _______, _______, KC_DOWN, KC_LCBR, KC_LPRN, KC_RPRN, KC_RCBR, KC_P1, KC_P2, KC_P3, KC_MINS, KC_END, \
-    _______, _______, KC_NO, _______, _______, KC_P0 \
+  [_LOWER] = LAYOUT_kc(
+    TILD, EXLM,   AT, HASH,  DLR, PERC,             CIRC, AMPR, ASTR, LPRN, RPRN, PGUP, \
+    ____,    1,    2,    3,    4,    5,                6,    7,    8,    9,    0, PGDN, \
+    ____, ____, LEFT, RGHT,   UP, LBRC,             RBRC,   P4,   P5,   P6, PLUS, HOME, \
+    ____, ____, ____, ____, DOWN, LCBR, LPRN, RPRN, RCBR,   P1,   P2,   P3, MINS,  END, \
+                            ____, ____, XXXX, ____, ____,   P0 \
   ),
 
-  [_RAISE] = LAYOUT(
-    KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, \
-    _______, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE, \
-    _______, KC_MPRV, KC_MNXT, KC_VOLU, KC_PGUP, RGB_SAD, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT, LALT(KC_SPC), _______, \
-    _______, KC_MSTP, KC_MPLY, KC_VOLD, KC_PGDN, KC_LPRN, KC_END, _______, KC_RPRN, KC_PLUS, KC_MINS, KC_EQL, KC_UNDS, _______, \
-    _______, _______, _______, KC_NO, _______, _______ \
+  [_RAISE] = LAYOUT_kc(
+      F12,   F1,   F2,   F3,   F4,   F5,               F6,   F7,   F8,   F9,  F10,  F11, \
+     ____, EXLM,   AT, HASH,  DLR, PERC,             CIRC, AMPR, ASTR, LPRN, RPRN, PIPE, \
+     ____, MPRV, MNXT, VOLU, PGUP, ____,             LEFT, DOWN,   UP, RGHT, ASPC, ____, \
+     ____, MSTP, MPLY, VOLD, PGDN, LPRN,  END, ____, RPRN, PLUS, MINS,  EQL, UNDS, ____, \
+                             ____, ____, ____, XXXX, ____, ____ \
   ),
 
   [_ADJUST] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, BL_TOGG, BL_STEP, BL_BRTG, BL_ON, BL_INC, RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, \
-    _______, _______, _______, _______, BL_OFF, BL_DEC, _______, _______, _______, RGB_RMOD, RGB_HUD, RGB_SAD, \
-    RGB_VAD, RGB_SPD, _______, _______, _______, _______, _______, _______ \
+    _______, _______, _______, _______, _______, _______,                   _______,  _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______,                   _______,  _______, _______, _______, _______, _______, \
+    _______, BL_TOGG, BL_STEP, BL_BRTG,   BL_ON,  BL_INC,                   RGB_TOG,  RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI, \
+    _______, _______, _______, _______,  BL_OFF,  BL_DEC, _______, _______, _______, RGB_RMOD, RGB_HUD, RGB_SAD, \
+                               RGB_VAD, RGB_SPD, _______, _______, _______, _______,  _______, _______ \
   )
 };
 
